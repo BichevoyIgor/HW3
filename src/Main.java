1e1ce748ed6de0481ee03d991eab22e61f92fd6d
BichevoyIgor <igoreek@mail.ru>
2022-01-29 10:49:18 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import counter.Counter;
+import counter.Increment;
+import pingPong.Ping;
+import pingPong.Pong;
+
 public class Main {
     public static void main(String[] args) {
+/*
+// Программа, в которой два потока поочередно пишут ping и pong
+        Object object = new Object();
+        Ping ping = new Ping(object);
+        Pong pong = new Pong(object);
+        ping.start();
+        pong.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ping.interrupt();
+        pong.interrupt();
+*/
+
+
+
+/*
+// Потокобезопасный счетчик с помощью интерфейса Lock.
+        Counter c = new Counter();
+        Increment i1 = new Increment(c);
+        Increment i2 = new Increment(c);
+        i1.start();
+        i2.start();
+        try {
+            Thread.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        i1.interrupt();
+        i2.interrupt();
+*/
+
 
     }
 }
+
